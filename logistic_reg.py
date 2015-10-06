82d13f833fa6b03b8cf7af69f3d20113cb424bc1
Tammo Behrends <behr.tam@gmail.com>
2015-10-06 15:06:40 +0200

    Changed dataset location

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,9 +1,10 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error
 
 import pandas as pd
 
-df = pd.read_csv('winequality-white.csv', header=0, sep=';')
+df = pd.read_csv('dataset/winequality-white.csv', header=0, sep=';')
 
 X = df[list(df.columns)[:-1]]
 y = df['quality']
@@ -14,4 +15,6 @@ model = LogisticRegression()
 model.fit(X_train, y_train)
 
 y_predict = model.predict(X_test)
-model.score(X_test, y_test)
+
+print 'Score:', model.score(X_test, y_test)
+print 'RMSE:', mean_squared_error(y_predict, y_test) ** 0.5
