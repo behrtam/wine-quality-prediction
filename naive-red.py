fd6154dbaef39b1027771f2b901060b2343dde01
Tim Unkrig <tim@open-tim.org>
2015-10-15 09:32:41 +0200

    add naive and svm files

---
+++ naive-red.py
@@ -0,0 +1,35 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+
+df = pd.read_csv('dataset/winequality-red.csv', header=0, sep=';')
+
+X = df[list(df.columns)[:-1]]
+y = df['quality']
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+modelg = GaussianNB()
+modelg.fit(X_train, y_train)
+y_predict = modelg.predict(X_test)
+print "GausseanNB Score:" + str(modelg.score(X_test, y_test))
+mse = mean_squared_error(y_predict, y_test)
+print "RMSE:" + str(mse ** 0.5)
+
+modelm = MultinomialNB()
+modelm.fit(X_train, y_train)
+y_predict = modelm.predict(X_test)
+print "MultinomialNB Score:" + str(modelm.score(X_test, y_test))
+mse = mean_squared_error(y_predict, y_test)
+print "RMSE:" + str(mse ** 0.5)
+
+modelb = BernoulliNB()
+modelb.fit(X_train, y_train)
+y_predict = modelb.predict(X_test)
+print "BernoulliNB Score: " + str(modelb.score(X_test, y_test))
+mse = mean_squared_error(y_predict, y_test)
+print "RMSE:" + str(mse ** 0.5)
---
+++ svm-white.py
@@ -0,0 +1,31 @@
+from sklearn import svm
+from sklearn.metrics import mean_squared_error
+from sklearn.cross_validation import train_test_split
+import pandas as pd
+import time
+
+tic = time.clock()
+
+df = pd.read_csv('dataset/winequality-white.csv', header=0, sep=';')
+
+X = df[list(df.columns)[:-1]]
+y = df['quality']
+print "1"
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+print "splitted"
+model_lin = svm.LinearSVC()
+model_rbf = svm.SVC()
+print "made model"
+model_lin.fit(X_train, y_train)
+model_rbf.fit(X_train, y_train)
+print "fitting"
+y_predict_lin = model_lin.predict(X_test)
+y_predict_rbf = model_rbf.predict(X_test)
+print "predicted"
+mse_lin = mean_squared_error(y_predict_lin, y_test)
+mse_rbf = mean_squared_error(y_predict_rbf, y_test)
+print "mserd"
+print model_lin.score(X_test, y_test), "RMSE: " + str(mse_lin ** 0.5)
+print model_rbf.score(X_test, y_test), "RMSE: " + str(mse_rbf ** 0.5)
+toc = time.clock()
+print  (toc - tic)
